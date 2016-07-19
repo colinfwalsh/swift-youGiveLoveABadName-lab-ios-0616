5edecf18183be50f5babb6551428ceffcca301a6
colinfwalsh <colinfwalsh@gmail.com>
2016-07-19 10:48:16 -0400

    Animation lab 2

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var resize: UIButton!
+    
+    @IBOutlet var picWindow: UIImageView!
+    
+    var didTransform = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,7 +22,44 @@ class ViewController: UIViewController {
     
     @IBAction func expandButtonTapped(sender: AnyObject) {
         
+        if self.didTransform == false {
+            animateEnlarge()
+            self.didTransform = true}
+        else {
+            animateDefault()
+            self.didTransform = false
+            }
+        
+        
+        
+        
     }
+    
+    func animateEnlarge() {
+        UIView.animateKeyframesWithDuration(0.25, delay: 0.0, options: [.CalculationModeLinear] , animations: {
+            
+            self.picWindow.transform = CGAffineTransformMakeScale(1.00, 0.95)
+            self.view.bringSubviewToFront(self.resize)
+            self.view.layoutIfNeeded()
 
+            
+        }) { (completed) in
+            UIView.animateKeyframesWithDuration(0.75, delay: 0.0, options: [.CalculationModeLinear], animations: {self.picWindow.transform = CGAffineTransformMakeScale(1.0, 2.00)}, completion: nil)
+            
+        }
+        
+    }
+    
+    func animateDefault() {
+        UIView.animateKeyframesWithDuration(0.25, delay: 0.0, options: [.CalculationModeLinear] , animations: {
+            
+            self.picWindow.transform = CGAffineTransformMakeScale(1.00, 2.15)
+            self.view.bringSubviewToFront(self.resize)
+            self.view.layoutIfNeeded()}) { (completed) in
+                UIView.animateKeyframesWithDuration(0.75, delay: 0.15, options: [.CalculationModeLinear], animations: {self.picWindow.transform = CGAffineTransformMakeScale(1.0, 1.0)}, completion: nil)
+        }
+        
+    }
+    
 }
 
